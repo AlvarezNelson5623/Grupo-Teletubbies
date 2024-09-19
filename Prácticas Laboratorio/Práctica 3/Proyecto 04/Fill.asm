d294baac8ee20ae98b6adc968072e636bb022f63
Sebastian Landinez <138395523+SebastianRC999@users.noreply.github.com>
2024-09-19 15:21:45 -0500

    Fill.asm

--- Prácticas Laboratorio/Práctica 3/Proyecto 04/Fill.asm
+++ Prácticas Laboratorio/Práctica 3/Proyecto 04/Fill.asm
@@ -14,22 +14,19 @@
 // See NOTES.md for details on how this works
 
 (PROGRAM_START)
-// Set R0 to SCREEN
 @SCREEN
 D=A
 @R0
 M=D
 
 (KEYBOARD_LOOP)
-
-// Read the Keyboard and set color
 @color
 M=0
 @KBD
 D=M
 @ENDKBDIF
 D;JEQ
-// If keyboard is pressed
+
 @0
 D=A
 D=D-1
@@ -37,19 +34,12 @@ D=D-1
 M=D
 (ENDKBDIF)
 
-// At this point @color = 0|-1 depending on whether a key is pressed
-
-// coloring code goes here
-
-// Read color to D
 
 @color
 D=M
 
-// Load the value in R0 to A
 @R0
 A=M
-// And off we go hunting
 M=D
 A=A+1
 M=D
@@ -114,15 +104,12 @@ M=D
 A=A+1
 M=D
 
-// Keyboard Loop ending
 
-// Bump A by 1 and write it to R0
 A=A+1
 D=A
 @R0
 M=D
 
-// If R0-24575 <= 0, then jump to program start
 @24575
 D=A
 @R0
@@ -130,8 +117,6 @@ D=M-D // (D=R0-24575)
 @ENDIF
 D;JLE
 
-// Here, R1 is negative
-// set R0=@SCREEN and let the loop continue
 @SCREEN
 D=A
 @R0
@@ -140,6 +125,6 @@ M=D
 
 (ENDIF)
 
-// OTHERWISE, we just restart the keyboard loop
+
 @KEYBOARD_LOOP
 0;JMP
